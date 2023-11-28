68b28e358a575e147a97a4df213ec2f25a0ddd18
Darek <darek1430143@gmail.com>
2023-11-28 19:47:32 +0100

    deleted useless files, started on website and controllers

---
+++ QuizBlazorApp/Server/Controllers/QuizController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizBlazorApp.Server.Data;
+
+namespace QuizBlazorApp.Server.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuizController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public QuizController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
